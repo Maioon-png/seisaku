216878aad602e0ce08ea1b01f7617a35adf388f5
Maioon-png <mi_mkb.12_5@icloud.com>
2024-10-22 23:06:15 +0900

    レビュー星をラジオボタンからクリック式へ

--- src/resources/views/review_add.blade.php
+++ src/resources/views/review_add.blade.php
@@ -60,6 +60,52 @@
 
             fileInput.click();
         }, false);
+
+        // 星評価
+        var stars = document.getElementsByClassName("star_mark");
+        var star_form = document.getElementById("star_form");
+        var clicked = false;
+        
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].addEventListener(
+            "mouseover",
+            () => {
+                if (!clicked) {
+                for (let j = 0; j <= i; j++) {
+                    stars[j].style.color = "blue";
+                }
+                }
+            },
+            false
+            );
+
+            stars[i].addEventListener(
+            "mouseout",
+            () => {
+                if (!clicked) {
+                for (let j = 0; j < stars.length; j++) {
+                    stars[j].style.color = "gray";
+                }
+                }
+            },
+            false
+            );
+
+            stars[i].addEventListener(
+            "click",
+            () => {
+                clicked = true;
+                for (let j = 0; j <= i; j++) {
+                stars[j].style.color = "blue";
+                }
+                for (let j = i + 1; j < stars.length; j++) {
+                stars[j].style.color = "gray";
+                }
+                star_form.value = i+1;
+            },
+            false
+            );
+        }
     });
 
     function previewFile(file) {
@@ -112,18 +158,13 @@
             <form method="POST" action="{{ url('/review/store') }}" enctype="multipart/form-data">
                 @csrf
                 <p class="comment_p">体験を評価してください</p>
-                <div class="star">
-                    <!-- 星ラジオボタン -->
-                    <input type="radio" id="star5" name="star" value=5 class="hidden peer">
-                    <label for="star5" class="">★</label>
-                    <input type="radio" id="star4" name="star" value=4 class="hidden peer">
-                    <label for="star4" class="">★</label>
-                    <input type="radio" id="star3" name="star" value=3 class="hidden peer">
-                    <label for="star3" class="">★</label>
-                    <input type="radio" id="star2" name="star" value=2 class="hidden peer">
-                    <label for="star2" class="">★</label>
-                    <input type="radio" id="star1" name="star" value=1 class="hidden peer">
-                    <label for="star1" class="">★</label>
+                <div class="star_box">
+                    <input type="hidden" id="star_form" name="star" value="">
+                    <span class="star_mark" id="1">★</span>
+                    <span class="star_mark" id="2">★</span>
+                    <span class="star_mark" id="3">★</span>
+                    <span class="star_mark" id="4">★</span>
+                    <span class="star_mark" id="5">★</span>
                 </div>
                 @error('star')
                 <p class='error_message'>{{$message}}</p>
